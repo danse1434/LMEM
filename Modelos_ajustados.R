996cfbcb850bf3a93de59240255564a17a84fb31
kdcloop <dsparrag@unal.edu.co>
2020-01-19 19:09:43 -0500

    Cambio en algunos componentes del script.

--- Modelos_ajustados.R
+++ Modelos_ajustados.R
@@ -1,44 +1,95 @@
-# 1. Creación de datos agrupados ------------------------------------------
-da = read.csv(file.choose()) # Escoger archivo datos.csv
-m = groupedData(Ln ~ dia | sujeto/grupo,
-                data=da,
-                labels=list(x="Dia de administración", y="Logaritmo de volumen tumoral"))
+##------------------------------------------------------------------------#
+## Nombre del Script: Análisis por Modelo de Efectos Mixtos Lineales ------
+##  
+## Proposito del Script: Ejemplo de análisis de modelo de regresión LMEM
+##  
+## Autor: Daniel S. Parra Gonzalez 
+## Fecha de creacion:  
+##  
+## Copyright (c) Daniel S. Parra, 2020 
+##  
+## Email: dsparrag@unal.edu.co 
+##------------------------------------------------------------------------#
+##########################################################################-
+# Carga de paquetes
+require(tidyverse)
+require(lme4)
+require(nlme)
 
-unique(getGroups(m))
-gsummary(m, invar=TRUE)
-plot(m, layout=c(2,2), between=list(x=c(1),y=c(1)), display=1, collapse = 1)
+##########################################################################-
+# Creación de datos agrupados ---------------------------------------------
+##########################################################################-
+# Lectura de archivo de datos
+data <- read_csv("Datos/Datos.csv", 
+                 col_types = cols(dia = col_integer(), 
+                                  grupo = col_factor(levels = c(1:4)), 
+                                  sujeto = col_factor(levels = c(1:28))))
+data <- data %>%
+  mutate(
+    label = case_when(
+      grupo == 1 ~ "Control",
+      grupo == 2 ~ "X 100mg/kg PO",
+      grupo == 3 ~ "X 10mg/kg PO",
+      grupo == 4 ~ "X 10mg/kg IP",
+      TRUE ~ NA_character_
+    )
+  )
+##########################################################################-
+# Crear objeto de datos agrupados
+data1 = groupedData(Ln ~ dia | sujeto / grupo, 
+                    data = data, 
+                    labels = list(x = "Tiempo (d)", 
+                                  y = "Log. Vol. Tumoral"))
 
-# Gráfico por sujeto experimental
-plot(m, displayLevel = 2)
-# Gráfico por grupo de tratamiento
-plot(m, display=1, collapse = 1, aspect=1, outer= ~ as.factor(grupo))
-plot(m, display=1, collapse = 2, aspect=1, outer= ~ as.factor(grupo), 
-     between=list(x=c(1),y=c(1)), FUN=mean(grupo)) # Sin leyenda
-
-unique(getCovariate(m))
+##########################################################################-
+# Visualizar grupos en objeto
+unique(getGroups(data1))
+gsummary(data1, invar=TRUE)
 
 
-# 2. Ajuste de modelos lineales en S con LM -------------------------------
-fm1.lme <- lm(Ln ~ peso + (as.factor(grupo) * dia),m)
-par( mfrow=c(2,2) )
+
+
+
+# G1 <- 
+  data1 %>% 
+  ggplot(mapping = aes(x = dia, y = Ln, group = sujeto, col = sujeto)) +
+  geom_point() +
+  facet_wrap(.~label, ncol = 2)+
+  geom_line() +
+  stat_summary(fun.y = mean, geom = 'line', lty = 'dashed', 
+               aes(group = grupo))
+  
+
+
+
+##########################################################################-
+# Ajuste de modelos lineales en S con LM ----------------------------------
+##########################################################################-
+# Modelo Lineal simple con peso y grupo
+fm1.lme <- lm(Ln ~ peso + (as.factor(grupo) * dia), data1)
+par(mfrow = c(2, 2))
 plot(fm1.lme)
 summary(fm1.lme)
 
-fm2.lme <- lm(Ln ~ 0 + peso + (grupo * dia),m)
-par( mfrow=c(2,2) )
+##########################################################################-
+# Modelo Lineal simple con peso y grupo, intercepto
+fm2.lme <- lm(Ln ~ 0 + peso + (grupo * dia), data1)
+par(mfrow = c(2, 2))
 plot(fm2.lme)
 summary(fm2.lme)
 
 # Boxplot de residuales por sujeto
-bwplot(getGroups(m)$sujeto ~ resid(fm2.lme),
-       main="Gráfico de Residuales por sujeto",
-       xlab="Valor de residuales")
-# Boxplot de residuales por grupo
-bwplot(getGroups(m)$grupo ~ resid(fm2.lme), col="green", 
-       main="Gráfico de Residuales por grupo de tratamiento",
-       xlab="Valor de residuales")
+lattice::bwplot(getGroups(data1)$sujeto ~ resid(fm2.lme),
+                main = "Gráfico de Residuales por sujeto",
+                xlab = "Valor de residuales")
 
-fm1.lis <- lmList(Ln ~ 0 + peso + (grupo * dia)|sujeto/grupo,m)
+# Boxplot de residuales por grupo
+lattice::bwplot(getGroups(data1)$grupo ~ resid(fm2.lme), col = "green", 
+                main = "Gráfico de Residuales por grupo de tratamiento", 
+                xlab = "Valor de residuales")
+
+# Regresión de lista con especificaciones
+fm1.lis <- lmList(Ln ~ 0 + peso + (grupo * dia) | sujeto / grupo, data1)
 summary(fm1.lis)
 
 # Correlación entre efectos
@@ -48,17 +99,29 @@ intervals(fm1.lis)
 # Gráfico de Residuales
 plot(intervals(fm1.lis))
 
-fm1.lme <- lme(Ln ~ 0 + peso + (grupo * dia),m ,
-               random = ~ 0 + dia|sujeto/grupo)
-fm1.lme.M <- lme(Ln ~ 0 + peso + (grupo * dia),m ,
+##########################################################################-
+# Modelo Linear Efectos Mixtos --------------------------------------------
+##########################################################################-
+
+fm1.lme <- lme(Ln ~ 0 + (peso * dia) + (grupo * dia), data1 , 
+               random = ~ 0 + dia | sujeto)
+
+
+
+
+
+fm1.lme <- lme(Ln ~ 0 + peso + (grupo * dia), data1 , 
+               random = ~ 0 + dia | sujeto / grupo)
+
+fm1.lme.data1 <- lme(Ln ~ 0 + peso + (grupo * dia),data1 ,
                random = ~ 0 + dia|sujeto/grupo, method="ML")
 
 fitted(fm1.lme, level=0:2) # valores predecidos
 
 par( mfrow=c(2,2) )
-plot(x=m$Ln,y=fitted(fm1.lme, level=0:2)[,1])
-plot(x=m$Ln,y=fitted(fm1.lme, level=0:2)[,2])
-plot(x=m$Ln,y=fitted(fm1.lme, level=0:2)[,3])
+plot(x=data1$Ln,y=fitted(fm1.lme, level=0:2)[,1])
+plot(x=data1$Ln,y=fitted(fm1.lme, level=0:2)[,2])
+plot(x=data1$Ln,y=fitted(fm1.lme, level=0:2)[,3])
 par( mfrow=c(1,1) )
 
 resid(fm1.lme, level=1, type="pearson")
@@ -68,9 +131,9 @@ plot(fm1.lme)
 compar.1 <- compareFits(coef(fm1.lis),coef(fm1.lme))
 plot(compar.1, mark=fixef(fm1.lme))
 
-#plot(comparePred(fm1.lis,fm1.lme, length.out=2, level=1, primary=m$dia))
+#plot(comparePred(fm1.lis,fm1.lme, length.out=2, level=1, primary=data1$dia))
 
-# Gráfico de Efectos Aleatorios de acuerdo a método de estimación (ML o REML)
-plot(compareFits(ranef(fm1.lme, level=1), ranef(fm1.lme.M, level=1)), mark=c(0,0))
+# Gráfico de Efectos Aleatorios de acuerdo a data1étodo de estimación (ML o REML)
+plot(compareFits(ranef(fm1.lme, level=1), ranef(fm1.lme.data1, level=1)), mark=c(0,0))
 
 
